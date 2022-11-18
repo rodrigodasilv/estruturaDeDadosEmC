13a96546d3dac9bd85b87baa817a65effc767b94
Rodrigo <55567123+rodrigodasilv@users.noreply.github.com>
2022-11-17 21:43:41 -0300

    Atualizado .h para conter cabeçalho de funções

--- ArvoreB/arvoreB.h
+++ ArvoreB/arvoreB.h
@@ -9,3 +9,15 @@ typedef struct arvoreB {
 	No* raiz;
 	int ordem;
 } ArvoreB;
+
+ArvoreB* criaArvore(int ordem);
+No* criaNo(ArvoreB* arvore);
+void percorreArvore(No* no, void (visita)(int chave));
+int localizaChave(ArvoreB* arvore, int chave);
+int pesquisaBinaria(No* no, int chave);
+No* localizaNo(ArvoreB* arvore, int chave);
+void adicionaChaveNo(No* no, No* direita, int chave);
+int transbordo(ArvoreB *arvore, No *no);
+No* divideNo(ArvoreB* arvore, No* no);
+void adicionaChave(ArvoreB* arvore, int chave);
+void adicionaChaveRecursivo(ArvoreB* arvore, No* no, No* novo, int chave);
